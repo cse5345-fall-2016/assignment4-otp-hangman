a64a546e4f7278ae1f7e0dc6fe6d8caede38601c
Jessica Yeh <jyeh@smu.edu>
2016-10-18 15:00:38 -0500

    added supervisor description

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -3,7 +3,16 @@ defmodule Hangman do
 
   @moduledoc """
 
-  Write your description of your supervision scheme here...
+  At the root of the supervisor tree is a Main supervisor that has two children: 
+  a Dictionary server, and a Game supervisor. The Game supervisor has the Game 
+  server as its child. The Main supervisor’s strategy is :rest_for_one, so if the 
+  Dictionary exits, both Dictionary and Game exit, but the Game exiting doesn’t 
+  affect the Dictionary. The restart option is :permanent, because we want the 
+  children to be restarted for any exit reason. The Game supervisor’s strategy 
+  is :one_for_one (although since there is only one child it doesn’t matter what 
+  we pick, but in case more Game servers are added as the supervisor’s children, 
+  one game crashing shouldn’t affect the other games). The restart option is 
+  :transient, because we only want it to restart if it crashes.
 
   """
 
