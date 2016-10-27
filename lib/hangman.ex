bdb36049531e371a2e99b32187c06470c8f2eaa7
Eric Zucho <ericzucho@gmail.com>
2016-10-27 13:59:01 -0500

    -> Comment block typo corrected.

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -3,7 +3,7 @@ defmodule Hangman do
 
   @moduledoc """
 
-  I chose the all_for_one supervison scheme because Dictionary will be the first child to be declared.
+  I chose the rest_for_one supervison scheme because Dictionary will be the first child to be declared.
   I want for the Game supervisor to be killed if the Dictionary module dies for some reason. If the Games supervisor
   dies, I have no reason to believe the Dictionary module should be forced to restart as well. Given that the Dictionary
   module's state doesn't change, I am assuming that if it is on a safe state when it is started, it will always be.
