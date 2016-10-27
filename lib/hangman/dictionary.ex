d963adae684b1e6daa4a66665a7b4fd2350cc6c0
Nariana <nsands@smu.edu>
2016-10-27 11:06:40 -0500

    updated dictionary.ex

--- lib/hangman/dictionary.ex
+++ lib/hangman/dictionary.ex
@@ -7,16 +7,35 @@ defmodule Hangman.Dictionary do
   # API #
   #######
 
+  @moduledoc """
+  We act as an interface to a wordlist (whose name is hardwired in the
+  module attribute `@word_list_file_name`). The list is formatted as
+  one word per line.
+  """
+
+  @word_list_file_name "assets/words.8800"
+
   def start_link(default \\ []) do
     GenServer.start_link(__MODULE__, default, name: @me)
   end
 
-  def set(key, value) do
-    GenServer.cast(@me, {:set, key, value})
+  @doc """
+  Return a random word from our word list. Whitespace and newlines
+  will have been removed.
+  """
+
+  @spec random_word() :: binary
+  def random_word do
+    GenServer.call(@me, {:random})
   end
 
-  def get(key) do
-    GenServer.call(@me, {:get, key})
+  @doc """
+  Return a list of all the words in our word list of a given length.
+  Whitespace and newlines will have been removed.
+  """
+  @spec words_of_length(integer)  :: [ binary ]
+  def words_of_length(len) do
+    GenServer.call(@me, {:length})
   end
 
   #########################
@@ -31,40 +50,18 @@ defmodule Hangman.Dictionary do
     {:noreply, Map.put(state, key, value)}
   end
 
-  def handle_call({:get, key}, _from, state) do
-    {:reply, state[key], state}
+  def handle_call(:random, _from, state) do
+    {:reply, word_list
+             |> Enum.random
+             |> String.trim, 
+    state}
   end
 
-
-  @moduledoc """
-  We act as an interface to a wordlist (whose name is hardwired in the
-  module attribute `@word_list_file_name`). The list is formatted as
-  one word per line.
-  """
-
-  @word_list_file_name "assets/words.8800"
-
-  @doc """
-  Return a random word from our word list. Whitespace and newlines
-  will have been removed.
-  """
-
-  @spec random_word() :: binary
-  def random_word do
-    word_list
-    |> Enum.random
-    |> String.trim
-  end
-
-  @doc """
-  Return a list of all the words in our word list of a given length.
-  Whitespace and newlines will have been removed.
-  """
-  @spec words_of_length(integer)  :: [ binary ]
-  def words_of_length(len) do
-    word_list
-    |> Stream.map(&String.trim/1)
-    |> Enum.filter(&(String.length(&1) == len))
+  def handle_call(:length, _from, state) do
+    {:reply, word_list
+             |> Stream.map(&String.trim/1)
+             |> Enum.filter(&(String.length(&1) == len)),
+    state}
   end
 
   ###########################
