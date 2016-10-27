dd1db79f6bdb3cfbd1de31798f2ad6021b6162a4
Sammy <sam.fastlane@gmail.com>
2016-10-27 13:28:14 -0500

    Make Dictionary a GenServer
    Implemented Dictionary API and corresponding server callbacks for
    original functions.

--- lib/hangman/dictionary.ex
+++ lib/hangman/dictionary.ex
@@ -1,4 +1,7 @@
 defmodule Hangman.Dictionary do
+  use GenServer
+
+  @me :dictionary
 
   @moduledoc """
   We act as an interface to a wordlist (whose name is hardwired in the
@@ -9,15 +12,20 @@ defmodule Hangman.Dictionary do
   @word_list_file_name "assets/words.8800"
 
   @doc """
+  starts and links a GenServer for dictionary processes
+  """
+  def start_link(default \\ word_list) do
+    GenServer.start_link(__MODULE__, default, name: @me)
+  end
+
+  @doc """
   Return a random word from our word list. Whitespace and newlines
   will have been removed.
   """
 
   @spec random_word() :: binary
   def random_word do
-    word_list
-    |> Enum.random
-    |> String.trim
+    GenServer.call(@me, { :random_word })
   end
 
   @doc """
@@ -26,11 +34,28 @@ defmodule Hangman.Dictionary do
   """
   @spec words_of_length(integer)  :: [ binary ]
   def words_of_length(len) do
-    word_list
-    |> Stream.map(&String.trim/1)
-    |> Enum.filter(&(String.length(&1) == len))
+    GenServer.call(@me, { :words_of_length, len})
   end
 
+  ## Server Callbacks
+
+  def init(word) do
+    {:ok, word}
+  end
+
+  def handle_call({ :random_word }, _from, state) do
+    word = word_list
+    |> Enum.random
+    |> String.trim
+    {:reply, word, state}
+  end
+ 
+  def handle_call({ :words_of_length, len }, _from, state) do
+    words = word_list
+    |> Stream.map(&String.trim/1)
+    |> Enum.filter(&(String.length(&1) == len))
+    {:reply, words, state}
+  end
 
   ###########################
   # End of public interface #
