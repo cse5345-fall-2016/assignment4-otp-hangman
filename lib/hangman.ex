f9aa9565bacf70472dacb85cbd669ccb8934d9a3
Jason Stumbaugh <jason.stumbaugh@orm-tech.com>
2016-10-29 11:40:39 -0500

    Create supervision strategy and application hierarchy

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -3,21 +3,30 @@ defmodule Hangman do
 
   @moduledoc """
 
-  Write your description of your supervision scheme here...
+  The supervision scheme I decided to implement consists of two supervisors: the
+  base Hangman supervisor and a GameSupervisor. The base supervisor has two
+  children: a Dictionary GenServer and the GameSupervisor. The strategy for the
+  base supervisor is 'rest_for_one', meaning the children after the crashed
+  process will be restarted. In this case, The dictionary server is started first
+  and the game supervisor second. If the Dictionary crashes, then the game
+  supervisor will be restarted. The dictionary will always be restarted, given
+  its 'permanent' restart option. The GameServerSupervisor has one child process:
+  the GameServer. It implements a 'one_for_one' supervision strategy, meaning if
+  the process dies, restart it and only it. The GameServer child process will
+  always be restarted if it crashes, due to its 'permanent' restart option.
 
   """
 
   def start(_type, _args) do
+    import Supervisor.Spec, warn: false
 
-    # Uncomment and complete this:
+    children = [
+      worker(Hangman.Dictionary, [], restart: :permanent),
+      supervisor(Hangman.GameServerSupervisor, [])
+    ]
 
-    # import Supervisor.Spec, warn: false
-    # 
-    # children = [
-    # ]
-    # 
-    # opts = [strategy: :you_choose_a_strategy, name: Hangman.Supervisor]
-    # Supervisor.start_link(children, opts)
+    opts = [strategy: :rest_for_one, name: Hangman.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 end
 
