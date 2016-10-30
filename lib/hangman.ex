639de523f4f3564461206ed8135feb469b3ed9ff
George (Zhiheng Li) <2433535844@qq.com>
2016-10-29 23:07:27 -0500

    Zhiheng Li's update
    Name: Zhiheng Li
    ID: 46945929

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -4,20 +4,25 @@ defmodule Hangman do
   @moduledoc """
 
   Write your description of your supervision scheme here...
-
+  In my own opinion I think the structure of this supervisor is use two supervisor.
+  The first is Root supervisor the task of this one is monitor the Dictionary module
+  and the Game.supervisor. So it had child supervisor and the strategy of Root
+  supervisor is rest_for_one. Also the Game.supervisor handle the Game Server
+  and the strategy is one_for_one.
   """
 
   def start(_type, _args) do
 
     # Uncomment and complete this:
 
-    # import Supervisor.Spec, warn: false
-    # 
-    # children = [
-    # ]
-    # 
-    # opts = [strategy: :you_choose_a_strategy, name: Hangman.Supervisor]
-    # Supervisor.start_link(children, opts)
+     import Supervisor.Spec, warn: false
+
+     children = [
+       worker(Hangman.Dictionary, [], id: :dictionary, restart: :permanent),
+       supervisor(GameServerSupervisor, [])
+     ]
+
+     opts = [strategy: :one_for_all, name: Hangman.Supervisor]
+     Supervisor.start_link(children, opts)
   end
 end
-
