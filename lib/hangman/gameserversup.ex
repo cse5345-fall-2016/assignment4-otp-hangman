007b4e8537375656edede397440fb874675e8dfe
Radhika Dharulkar <Radhika Dharulkar>
2016-10-29 20:42:03 -0500

    no errors

--- lib/hangman/gameserversup.ex
+++ lib/hangman/gameserversup.ex
@@ -1,27 +1,12 @@
-defmodule GameSup do
-  use Application
-
-  @moduledoc """
+defmodule GameSup.SubSupervisor do
+    use Supervisor
+    
+    @moduledoc """
 
   Write your description of your supervision scheme here...
 
   """
   
-  def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-     
-    children = [
-        worker(Hangman.GameServer, [])
-    ]
-    
-    opts = [strategy: :one_for_one, name: GameSup.Supervisor]
-    
-	Supervisor.start_link(children, opts)
-  end
-end
-
-defmodule GameSup.SubSupervisor do
-    use Supervisor
     def start_link() do
         {:ok, _pid} = Supervisor.start_link()
     end
