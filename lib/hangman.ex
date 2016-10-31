6fd8d70a66e115dcea37cbe04e04b0e24c9a55dc
Luke Oglesbee <lukewoglesbee@gmail.com>
2016-10-30 23:15:09 -0500

    Passes tests.

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -3,21 +3,29 @@ defmodule Hangman do
 
   @moduledoc """
 
-  Write your description of your supervision scheme here...
+  In our architecture the game depends on the dictionary but the
+  dictionary is indpendent of the game. We use the rest_for_one strategy
+  at the top level because we want all games to restart if there's a
+  problem with the dictionary. We have the game supervisor is then
+  responsible for managing the individual games.
+
+  The game supervisor uses a one_for_one strategy, restarting games
+  individually. I went with this strategy at the game level because
+  games are independent. A breakage in one does not change anything
+  in the other game processes.
 
   """
 
   def start(_type, _args) do
-
-    # Uncomment and complete this:
-
-    # import Supervisor.Spec, warn: false
-    # 
-    # children = [
-    # ]
-    # 
-    # opts = [strategy: :you_choose_a_strategy, name: Hangman.Supervisor]
-    # Supervisor.start_link(children, opts)
+    import Supervisor.Spec, warn: false
+    
+    children = [
+      worker(Hangman.Dictionary, [], id: :dictionary, restart: :transient),
+      supervisor(Hangman.GameSupervisor, []),
+    ]
+    
+    opts = [strategy: :rest_for_one, name: Hangman.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 end
 
--- lib/hangman/dictionary.ex
+++ lib/hangman/dictionary.ex
@@ -1,4 +1,5 @@
 defmodule Hangman.Dictionary do
+  use GenServer
 
   @moduledoc """
   We act as an interface to a wordlist (whose name is hardwired in the
@@ -6,18 +7,21 @@ defmodule Hangman.Dictionary do
   one word per line.
   """
 
+  @name :dictionary
   @word_list_file_name "assets/words.8800"
+
+  
+  def start_link(state \\ []) do
+    GenServer.start_link(__MODULE__, state, name: @name)
+  end
 
   @doc """
   Return a random word from our word list. Whitespace and newlines
   will have been removed.
   """
-
   @spec random_word() :: binary
   def random_word do
-    word_list
-    |> Enum.random
-    |> String.trim
+    GenServer.call(@name, { :random_word })
   end
 
   @doc """
@@ -26,17 +30,34 @@ defmodule Hangman.Dictionary do
   """
   @spec words_of_length(integer)  :: [ binary ]
   def words_of_length(len) do
-    word_list
-    |> Stream.map(&String.trim/1)
-    |> Enum.filter(&(String.length(&1) == len))
+    GenServer.call(@name, { :words_of_length, len })
   end
 
+  def handle_call( { :random_word }, _from, state) do
+    { :reply , _random_word, state }
+  end
+
+  def handle_call( {:words_of_length, len}, _from, state) do
+    { :reply , _words_of_length(len), state }
+  end
 
   ###########################
   # End of public interface #
   ###########################
 
-  defp word_list do
+  defp _random_word() do
+    _word_list
+    |> Enum.random
+    |> String.trim
+  end
+
+  defp _words_of_length(len) do
+    _word_list
+    |> Stream.map(&String.trim/1)
+    |> Enum.filter(&(String.length(&1) == len))
+  end
+
+  defp _word_list do
     @word_list_file_name
     |> File.open!
     |> IO.stream(:line)
