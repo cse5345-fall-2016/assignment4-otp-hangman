e8dc14eb92f988e6d6788ed2f2fc61a3008b7950
Matthew Lee <matthew.lee711@gmail.com>
2016-10-30 23:22:35 -0500

    Added sub supervisor code and supervisor scheme

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -12,6 +12,21 @@ defmodule Hangman do
   temp directory
   mix new --sup hangman
 
+  Supervisor scheme:
+  
+  The top level supervisor monitors the game's Dictionary and SubSupervisor
+  A rest_for_one strategy for the the main supervisor is used because it will
+  restart both the dictionary and Game when the dictionary crashes. However, the
+  Game will not have any effect on the dictionary.
+
+  The SubSupervisor is using a :one_for_one strategy so if the game crashes,
+  it should not affect other games. Also it is monitoring only one child.
+
+  Dictionary is using a :permanent restart so any exits will trigger a restart.
+  Subsupervisor is using a transient restart because the process should restart
+  if the game crashes.
+
+
   """
 
   def start(_type, _args) do
@@ -21,10 +36,11 @@ defmodule Hangman do
     import Supervisor.Spec, warn: false
 
     children = [
-      worker(Hangman.Dictionary, [])
+      worker(Hangman.Dictionary, [], restart: :permanent),
+      worker(Hangman.SubSupervisor, [], restart: :transient)
     ]
 
-    opts = [strategy: :one_for_all, name: __MODULE__]
+    opts = [strategy: :rest_for_one, name: __MODULE__]
     Supervisor.start_link(children, opts)
   end
 end
